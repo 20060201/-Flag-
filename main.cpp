d96119c0333e29f6c0f7f7eb984f949316247c36
Mira <a750387@stdmail.nssh.ntpc.edu.tw>
2021-01-03 10:13:02 +0000

    01

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ int main() {
     int R, C; //學生隊形有幾列與幾行
     int total = 0;
     cin >> R >> C;
+
     int line[R][C];
 
     for(int i = 0; i < R; i++) {
@@ -12,20 +13,32 @@ int main() {
             cin >> line[i][j];
         }
     }
-
+    
     for(int i = 0; i < R; i++) {
         for(int j = 0; j < C; j++) {
-            if(line[i][j] == line[i - 1][j] && line[i + 1][j]) {
-                if(line[i][j] == line[i][j - 1] && line[i][j +1]) {
-                    if(line[i][j] == line[i + 1][j - 1] && line[i - 1][j - 1] \
-                    && line[i + 1][j + 1] && line[i - 1][j + 1]) {
-                    } else {
-                        total ++;
+            //cout << line[i][j]  << " ";
+        }
+        //cout << endl;
+    }
+
+    bool bCheck;
+    int num[8][2]={{-1, 0}, {1, 0}, {0, -1}, {0, 1}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
+    for(int i = 0; i < R; i++) {
+        for(int j = 0; j < C; j++) {
+            bCheck = true;
+            for(int k = 0; k < 8; k++) {
+                if( i+num[k][0] >=0 && i+num[k][0] < R && j+num[k][1] >=0 && j+num[k][1] < C) {
+                    if (line[i][j] == line[i+num[k][0]][j+num[k][1]]) {
+                        bCheck =false;
                     }
                 }
             }
+            if (bCheck) {
+                total++;
+                //cout << i << " " << j << " " << line[i][j] << " " << endl;
+            }
         }
     }
-
-    cout << total;
+    
+    cout << total << endl;
 }
